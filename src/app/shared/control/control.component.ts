fec167bd08370f3d08786d6508699dc9c56a8c1b
sibulele10 <sibulelem2@discovery.co.za>
2026-07-18 11:28:39 +0200

    listen to events via host

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,8 +9,13 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'control',
+    '(click)': 'onClick()',
   },
 })
 export class ControlComponent {
   public label = input.required<string>();
+
+  public onClick() {
+    console.log('Clicked!');
+  }
 }
