b6b849021e989a2dbdefdb21e75828ef984ccef9
sibulele10 <sibulelem2@discovery.co.za>
2026-09-13 12:23:52 +0200

    access template elements

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,9 +11,9 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  onSubmit(title: string, ticketText: string) {
+  onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
     console.dir(title);
     console.dir(ticketText);
-
+    form.reset();
   }
 }
